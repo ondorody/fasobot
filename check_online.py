e5f58931fbd6ee306f5b4884dab228156ac574cf
ondorody <37157793+ondorody@users.noreply.github.com>
2019-06-11 18:03:16 +0000

    Update check_online.py

--- check_online.py
+++ check_online.py
@@ -2,7 +2,7 @@
 import requests,traceback,json
 param = {}
 param['q'] = "SELECT uid, name, pic_square, online_presence FROM user WHERE online_presence IN ('active', 'idle') AND uid IN (SELECT uid2 FROM friend WHERE uid1 = me())"
-param['access_token'] = 'EAAKkdOyVdMIBAMitYxUd3dmoRg1dBa8C8PeZAoKmU7rM6Hh2GiXFrEzpOeMRKJZCQddt3KoaXMQVdyAbcZAcPVwloNlw9mHEshFAsQBLQOl4sciruwDJh6MgW1ZBGLTDbZALmk42BO5fsWCsOnynG71Kgftv4j8MD48nRpzhEwAZDZD'
+param['access_token'] = 'EAAGCbeaSIQwBALZB3GZCo4CpTHrg6NIBkPQLZB6jZAnaHvMVeZAbx8U4uwHenMayiYHyjcpwXdxCCzY5htD6wvp96fI4jAZBpHjACDZAhoQwsz6t0xWg3Dp7DZBOtWuonkKDmgp9Dbv7Tr2oMPw0fVmy7ZCTdqZB22ZCZC5ZB0ULAYbRJN1JTGTdD2wiT'
 param['method'] = 'GET'
 try:
   response = requests.get("https://graph.facebook.com/fql%22,params=param")
